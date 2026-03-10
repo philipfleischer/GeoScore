048b82a2715436034460e43ab3bd53c5795adfc3
jeryosa <jeryosa@ulrik.uio.no>
2026-03-10 13:12:24 +0100

    overføring av kode

--- app/src/main/java/no/uio/ifi/in2000/team20/team20app/data/model/ForecastDto.kt
+++ app/src/main/java/no/uio/ifi/in2000/team20/team20app/data/model/ForecastDto.kt
@@ -1,2 +1,39 @@
 package no.uio.ifi.in2000.team20.team20app.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class WeatherResponseDto(
+    val properties: PropertiesDto
+)
+
+@Serializable
+data class PropertiesDto(
+    val timeseries: List<TimeseriesDto>
+)
+
+@Serializable
+data class TimeseriesDto(
+    val time: String,
+    val data: DataDto
+)
+
+@Serializable
+//kan være null
+data class DataDto(
+    val instant: InstantDto?,
+
+    )
+
+@Serializable
+//kan være null
+data class InstantDto(
+    val details: InstantDetailsDto?,
+)
+
+@Serializable
+data class InstantDetailsDto (
+    val air_temperature: Float,
+    val relative_humidity: Float,
+    val wind_speed: Float
+)
