07b7a48374a1bf68fef4bb351067cc130de9609c
David Hovde <davidhov@ifi.uio.no>
2026-03-21 02:44:35 +0100

    Added stuff to .gitignore. Changed config to read from secrets.properties. Should migrate this over to using secrets plugin rather than hardcoded declarations, but this is a minor detail.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
 buildscript {
     dependencies {
